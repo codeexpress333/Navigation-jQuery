c6bff71411f92eda946d86d286087860847897bc
code express <codeexpress333@gmail.com>
2022-09-25 09:05:37 +0530

    navigation

---
+++ main.js
@@ -0,0 +1,7 @@
+$('.openNav').click(function () {
+    $('body').toggleClass('navOpen');
+    $('nav').toggleClass('open');
+    $('.wrapper').toggleClass('open');
+    $(this).toggleClass('open');
+});
+
